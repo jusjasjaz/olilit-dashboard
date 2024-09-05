d3a462bdb26d4ada9082cf8c64d03b23902dca19
Jazmine Lean Dimagiba <jazminedimagiba3@gmail.com>
2024-09-06 02:55:18 +0800

    update sign up step success

--- src/app/account/signup-completion/sign-up-step-success/sign-up-step-success.component.ts
+++ src/app/account/signup-completion/sign-up-step-success/sign-up-step-success.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up-step-success',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpStepSuccessComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  signIn() {
+    this.router.navigate(['/auth/login']);
+  }
+
+  next() {
+    this.router.navigate(['/auth/login']);
   }
 
 }
